4062d94b9f529a2cf964054e6c5e394148c11ed3
Mohit261014 <56679386+Mohit261014@users.noreply.github.com>
2020-06-30 12:58:45 +0530

    Add files via upload

---
+++ flags.js
@@ -0,0 +1,98 @@
+function brazilfunc(){
+    var c = "This is the flag of BRAZIL";
+    document.getElementById("brazil").innerHTML = c;
+}
+function outbrazil(){
+    var c = ""
+    document.getElementById("brazil").innerHTML = c;
+}
+function argfunc(){
+    var c = "This is the flag of ARGENTINA";
+    document.getElementById("arg").innerHTML = c;
+}
+function outarg(){
+    var c = ""
+    document.getElementById("arg").innerHTML = c;
+}
+function indiafunc()
+{
+var c = "This is the flag of INDIA";
+document.getElementById("india").innerHTML = c;
+}
+function outindia(){
+    var c = ""
+    document.getElementById("india").innerHTML = c;
+}
+function usafunc()
+{
+var c = "This is the flag of USA";
+document.getElementById("usa").innerHTML = c;
+}
+function outusa(){
+    var c = ""
+    document.getElementById("usa").innerHTML = c;
+}
+function japanfunc()
+{
+var c = "This is the flag of JAPAN";
+document.getElementById("japan").innerHTML = c;
+}
+function outjapan(){
+    var c = ""
+    document.getElementById("japan").innerHTML = c;
+}
+function srilankafunc()
+{
+var c = "This is the flag of SRI-LANKA";
+document.getElementById("srilanka").innerHTML = c;
+}
+function outsrilanka(){
+    var c = ""
+    document.getElementById("srilanka").innerHTML = c;
+}
+function northkoreafunc()
+{
+var c = "This is flag of NORTH-KOREA";
+document.getElementById("northkorea").innerHTML = c;
+}
+function outnorthkorea(){
+    var c = ""
+    document.getElementById("northkorea").innerHTML = c;
+}
+function spainfunc()
+{
+var c = "This is the flag of SPAIN";
+document.getElementById("spain").innerHTML = c;
+}
+function outspain(){
+    var c = ""
+    document.getElementById("spain").innerHTML = c;
+}
+function irelandfunc()
+{
+var c = "This is the flag of IRELAND";
+document.getElementById("ireland").innerHTML = c;
+}
+function outireland(){
+    var c = ""
+    document.getElementById("ireland").innerHTML = c;
+}
+function icelandfunc()
+{
+var c = "This is the flag of ICELAND";
+document.getElementById("iceland").innerHTML = c;
+}
+function outiceland(){
+    var c = ""
+    document.getElementById("iceland").innerHTML = c;
+}
+function finlandfunc()
+{
+var c = "This is the flag of FINLAND";
+document.getElementById("finland").innerHTML = c;
+}
+function outfinland(){
+    var c = ""
+    document.getElementById("finland").innerHTML = c;
+}
+
